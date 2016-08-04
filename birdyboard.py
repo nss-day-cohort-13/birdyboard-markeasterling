c5d486784f2415c002807cf3a7d730dc76af17cc
Mark Easterling <marke.easterling@gmail.com>
2016-08-03 20:00:31 -0500

    user creation tests passing

--- birdyboard.py
+++ birdyboard.py
@@ -1,4 +1,7 @@
-class BirdyBoard
+import uuid
+
+
+class Birdyboard:
 
     def __init__(self):
         self.user_list = []
@@ -6,4 +9,15 @@ class BirdyBoard
         self.conversation_list = []
         self.current_user = None
 
+    class User:
 
+        def __init__(self, fullname, screenname):
+            self.screen_name = screenname
+            self.full_name = fullname
+            self.unique_user_ID = str(uuid.uuid1())
+            print(self.unique_user_ID)
+
+
+# if __name__ == '__main__':
+#     birdyboard = BirdyBoard()
+#     birdyboard.user = birdyboard.User("dave", "davesauce")
--- test-user.py
+++ test-user.py
@@ -1,16 +1,18 @@
 import unittest
+
 from birdyboard import *
 
 
 class TestUser(unittest.TestCase):
 
     def test_user_creation(self):
-        self.user = User("bob vila", "bob_the_builder")
+        self.birdy = Birdyboard()
+        self.birdy.user = self.birdy.User("bob vila", "bob_the_builder")
 
-        self.assertEqual("bob vila", user.full_name)
-        self.assertEqual("bob_the_builder", user.screen_name)
-        self.assertIsNotNone(user.user_ID)
-        self.assertIsInstance(user, User)
+        self.assertEqual("bob vila", self.birdy.user.full_name)
+        self.assertEqual("bob_the_builder", self.birdy.user.screen_name)
+        self.assertIsNotNone(self.birdy.user.unique_user_ID)
+        self.assertIsInstance(self.birdy.user, self.birdy.User)
 
 
 if __name__ == '__main__':
